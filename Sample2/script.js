07af6ed21b100ae50b2a79488b6324faaa6cf1e4
ranjan3nov <ranjan3nov@gmail.com>
2023-08-06 17:57:51 +0530

    Copyright is added input group is used and fixed

--- Sample2/script.js
+++ Sample2/script.js
@@ -97,3 +97,17 @@ $(".submit").click(function () {
         }
     });
 })
+$(document).ready(function () {
+    $(".toggle-password").click(function () {
+        var passwordInput = $("#password");
+        var icon = $(this).find("i");
+
+        if (passwordInput.attr("type") === "password") {
+            passwordInput.attr("type", "text");
+            icon.removeClass("fa-eye-slash").addClass("fa-eye");
+        } else {
+            passwordInput.attr("type", "password");
+            icon.removeClass("fa-eye").addClass("fa-eye-slash");
+        }
+    });
+});
